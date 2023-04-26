189068d206a22ed9ea8c56e6fb05ed24c4074629
Kyu Yeon Kim <58721219+kykim102@users.noreply.github.com>
2023-04-27 01:40:23 +1200

    End of  section 5 Error Handling

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,8 @@ import { Container, ThemeProvider, createTheme } from '@mui/material'
 import Header from './Header'
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App () {
   const [darkMode, setDarkMode] = useState(false)
@@ -19,6 +21,7 @@ function App () {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
       <CssBaseline />
       <Header setDarkMode={setDarkMode} />
       <Container>
